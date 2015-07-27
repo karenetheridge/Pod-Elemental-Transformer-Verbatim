11a79505f14fd2971c5465a1df913b9d38653cfd
Karen Etheridge <ether@cpan.org>
2015-07-26 19:13:43 -0700

    initial code, docs, tests

--- lib/Pod/Elemental/Transformer/Verbatim.pm
+++ lib/Pod/Elemental/Transformer/Verbatim.pm
@@ -1,12 +1,80 @@
 use strict;
 use warnings;
 package Pod::Elemental::Transformer::Verbatim;
-# ABSTRACT: ...
-# KEYWORDS: ...
+# ABSTRACT: Transform :verbatim regions into verbatim paragraphs
+# KEYWORDS: pod transformer verbatim literal indent code text
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
 our $VERSION = '0.001';
 
+use Moose;
+with 'Pod::Elemental::Transformer' => { -version => '0.101620' };
+
+use Pod::Elemental::Types 'FormatName';
+use Pod::Elemental::Element::Pod5::Verbatim;
+use namespace::autoclean;
+
+has format_name => (
+    is  => 'ro',
+    isa => FormatName,
+    default => 'verbatim',
+);
+
+sub transform_node
+{
+    my ($self, $node) = @_;
+
+    # why do we process in reverse? it should make no difference
+    for my $i (reverse(0 .. $#{ $node->children })) {
+        my $para = $node->children->[ $i ];
+        next unless $self->__is_xformable($para);
+        my @replacements = $self->_make_verbatim( $para );
+        splice @{ $node->children }, $i, 1, @replacements;
+    }
+
+    return $node;
+}
+
+sub _make_verbatim
+{
+    my ($self, $parent) = @_;
+
+    # TODO: we can make this nicer with a simple map.
+    my @replacements;
+
+    PARA: for my $para (@{ $parent->children }) {
+        unless ($para->isa('Pod::Elemental::Element::Pod5::Ordinary')) {
+            push @replacements, $self->__is_xformable($para)
+                ? $self->_make_verbatim($para)
+                : $para;
+
+            next PARA;
+        }
+
+        if (length $para->content)
+        {
+            push @replacements, Pod::Elemental::Element::Pod5::Verbatim->new({
+                content => join("\n", map { '    ' . $_ } split(/\n/, $para->content)),
+              });
+        }
+    }
+
+    return @replacements;
+}
+
+# from ::List
+sub __is_xformable
+{
+    my ($self, $para) = @_;
+
+    return unless $para->isa('Pod::Elemental::Element::Pod5::Region')
+        and $para->format_name eq $self->format_name;
+
+    confess('verbatim regions must be pod (=begin :' . $self->format_name . ')')
+        unless $para->is_pod;
+
+    return 1;
+}
 
 1;
 __END__
@@ -15,19 +83,53 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Pod::Elemental::Transformer::Verbatim;
+In your F<weaver.ini>:
 
-    ...
+     [-Transformer / Verbatim]
+     transformer = Verbatim
+
+Or in a plugin bundle:
+
+    sub mvp_bundle_config {
+        return (
+            ...
+            [ '@Author::YOU/Verbatim', _exp('-Transformer'), { 'transformer' => 'Verbatim' } ],
+        );
+    }
 
 =head1 DESCRIPTION
 
-...
+This module acts as a L<pod transformer|Pod::Elemental::Transformer>, using
+C<:verbatim> regions to mark sections of text for treatment as if they were
+verbatim paragraphs. The transformer indents the contained text by four
+columns, allowing the pod parser to treat them properly; see
+L<perlpod/"Verbatim Paragraph">.
 
-=head1 FUNCTIONS/METHODS
+That is, this pod:
 
-=head2 C<foo>
+    =begin :verbatim
 
-...
+    Here is some text.
+
+    =end :verbatim
+
+Is transformed to:
+
+        Here is some text
+
+Note that a single paragraph can be simply noted with C<=for :verbatim> rather
+than using C<=begin :verbatim> and C<=end :verbatim> lines.
+
+=attr format_name
+
+This attribute (defaulting to C<verbatim>) is the region format that will be
+processed by this transformer.
+
+=method transform_node
+
+Given a pod document object, returns the object with all its children with
+C<:verbatim> directives removed and appropriate content replaced with
+L<Pod::Elemental::Element::Pod5::Verbatim> objects.
 
 =head1 SUPPORT
 
@@ -37,13 +139,9 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Pod-Elemental-Transformer-Verbatim@rt.cpan.org|mailto:bug-Pod-Elemental-Transformer-Verbatim@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =for :list
-* L<foo>
+* L<Pod::Weaver>
 
 =cut
